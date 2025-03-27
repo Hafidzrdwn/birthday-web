6aaf7caff15f644fc87e77014ee804bb5766a632
Hafidzrdwn <hafidz10032005@gmail.com>
2025-03-28 02:30:02 +0700

    fix: fix typo at calendar section

--- app/page.tsx
+++ app/page.tsx
@@ -639,7 +639,7 @@ export default function BirthdayPage() {
                         </div>
                         <div className="flex items-center gap-2">
                           <div className="w-2 h-2 rounded-full bg-blue-500"></div>
-                          <p className="text-gray-700">A Wondeful Day!</p>
+                          <p className="text-gray-700">A Wonderful Day!</p>
                         </div>
                       </div>
                     </motion.div>
